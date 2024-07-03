f6e9b8439fd279695d130c2f946cb32c5502c91c
AbreuMajanoRoger <140257317+AbreuMajanoRoger@users.noreply.github.com>
2024-07-03 22:24:52 +0200

    agregado de funciones y el adapter

--- app/src/main/java/com/example/myapplication/NotesAdapter.kt
+++ app/src/main/java/com/example/myapplication/NotesAdapter.kt
@@ -1,7 +1,85 @@
 package com.example.myapplication
 
 import android.content.Context
+import android.content.Intent
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.PopupMenu
+import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
+import com.example.myapplication.databinding.NoteItemBinding
 
 
-//class NotesAdapter (private var notes: MutableList<Note>, context: Context):RecyclerView.Adapter<>(){ }
+class NotesAdapter (private var notes: MutableList<Note>, context: Context):RecyclerView.Adapter<NotesAdapter.NoteViewHolder>(){
+
+
+    class NoteViewHolder(private val binding: NoteItemBinding):
+            RecyclerView.ViewHolder(binding.root){
+                val titleTextView: TextView = binding.titleTextView
+                val contenTextView: TextView = binding.contenTextView
+                val optionButton: ImageView = binding.optionsButton
+            }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoteViewHolder {
+      val binding = NoteItemBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+        return NoteViewHolder(binding)
+    }
+
+    override fun getItemCount(): Int = notes.size
+
+    override fun onBindViewHolder(holder: NoteViewHolder, position: Int) {
+    val note = notes[position]
+        holder.titleTextView.text = note.title
+        holder.contenTextView.text = note.content
+
+        holder.optionButton.setOnClickListener {
+            val popupMenu = PopupMenu(holder.itemView.context, holder.optionButton)
+            popupMenu.inflate(R.menu.menu_item_notes)
+
+            popupMenu.setOnMenuItemClickListener {menuItem ->
+                when(menuItem.itemId){
+                    R.id.editOption ->{
+                        val intent = Intent(holder.itemView.context,UpdateActivity::class.java).apply {
+                            putExtra("note_id, note.id")
+                        }
+                        holder.itemView.context.startActivity(intent)
+                        true
+                    }
+
+                    R.id.deleteOption ->{
+                        db.deleteNote(note.id)
+                        removeItem(holder.adapterPosition)
+                        Toast.makeText(holder.itemView.context,"Note Deleted", Toast.LENGTH_SHORT).show()
+                        true
+                    }
+                    else -> false
+                }
+
+            }
+
+
+
+
+
+        }
+
+
+
+
+
+
+    }
+
+    fun refreshData(newNotes: List<Note>){
+
+        notes.clear()
+        notes.addAll(newNotes)
+        notifyDataSetChanged()
+
+    }
+
+
+}
+
--- app/src/main/java/com/example/myapplication/NotesDataBaseHelper.kt
+++ app/src/main/java/com/example/myapplication/NotesDataBaseHelper.kt
@@ -61,4 +61,43 @@ class NotesDataBaseHelper (context: Context): SQLiteOpenHelper(context,
         return noteliste
     }
 
+    fun updateNote(note:Note){
+        val db = writableDatabase
+        val values = ContentValues().apply {
+            put (COLUMN_TITLE, note.title)
+            put(COLUMN_CONTENT, note.content)
+        }
+
+        val whereClause = "$COLUMN_ID"
+        val  whereArgs = arrayOf(note.id.toString())
+        db.update(TABLE_NAME, values, whereClause, whereArgs)
+        db.close()
+
+    }
+
+    fun getNoteById(noteId : Int): Note{
+        val db = readableDatabase
+        val querry = "SELECT *FROM $TABLE_NAME WHERE $COLUMN_ID = $noteId"
+        val cursor = db.rawQuery(querry, null)
+        cursor.moveToFirst()
+
+        val id = cursor.getInt(cursor.getColumnIndexOrThrow(COLUMN_ID))
+        val title = cursor.getString(cursor.getColumnIndexOrThrow(COLUMN_TITLE))
+        val content = cursor.getString(cursor.getColumnIndexOrThrow(COLUMN_CONTENT))
+
+        cursor.close()
+        db.close()
+        return Note(id, title, content)
+    }
+
+
+
+
+
+
+
+
+
+
+
 }
