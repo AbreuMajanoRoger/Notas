3f0b32ba9d05a25fc40b4121cec345fd822354da
roger <abreumajanodev@gmail.com>
2024-07-03 13:53:26 +0200

    programa de notas, falta por modificar el adapter

---
+++ app/src/main/java/com/example/myapplication/ActivityAddNotes.kt
@@ -0,0 +1,32 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.example.myapplication.databinding.ActivityAddNotesBinding
+import com.example.myapplication.databinding.ActivityMainBinding
+
+class ActivityAddNotes : AppCompatActivity() {
+
+
+    private lateinit var binding: ActivityAddNotesBinding
+    private lateinit var  db:NotesDataBaseHelper
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        binding = ActivityAddNotesBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        db =NotesDataBaseHelper(this)
+
+        binding.saveButton.setOnClickListener {
+            val title = binding.titleEditText.text.toString()
+            val content = binding.contentEditText.text.toString()
+            val note = Note(0, title,content)
+            db.insertNote(note)
+            finish()
+            Toast.makeText(this,"NOTA guardada", Toast.LENGTH_SHORT).show()
+
+         }
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/NotesAdapter.kt
@@ -0,0 +1,7 @@
+package com.example.myapplication
+
+import android.content.Context
+import androidx.recyclerview.widget.RecyclerView
+
+
+//class NotesAdapter (private var notes: MutableList<Note>, context: Context):RecyclerView.Adapter<>(){ }
